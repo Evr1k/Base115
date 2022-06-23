d6bf28e92424c1bc4dd4f45d0c1b5e4c43461cab
Viktor Plekhanov <evr1k@mail.ru>
2022-06-23 19:26:19 +0500

    fix bag command 40.301

--- data_base/sql_db.py
+++ data_base/sql_db.py
@@ -9,12 +9,13 @@ def sql_start():
     if base:
         print('Data base connected - OK')
 
+
 async def get_all_details(message):
     for ret in cur.execute('SELECT DISTINCT Деталь FROM details_operation').fetchall():
         await bot.send_message(message.from_user.id, *ret)
 
+
 async def get_time_operation(message):
-    for ret in cur.execute('''
-                            SELECT Деталь, "время от БТЗ"  FROM details_operation 
-                            WHERE Деталь = "148.40.301" AND "время от БТЗ" IS NOT NULL''').fetchall():
-        await bot.send_message(message.from_user.id, *ret)
+    for ret in cur.execute(
+            'SELECT "номер операции", "время от БТЗ"  FROM details_operation WHERE Деталь = "148.40.301" AND "время от БТЗ" IS NOT NULL').fetchall():
+        await bot.send_message(message.from_user.id, f'Операция {ret[0]} - {ret[1]} минут')
