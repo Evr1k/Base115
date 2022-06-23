fb960016f38cb921d7247c33634cc5169a24bf3e
Viktor Plekhanov <evr1k@mail.ru>
2022-06-23 23:50:31 +0500

    update command details

--- data_base/sql_db.py
+++ data_base/sql_db.py
@@ -1,21 +1,32 @@
 import sqlite3 as sq
+from sqlite3 import Connection
+
 from loader import bot
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 def sql_start():
     global base, cur
-    base = sq.connect('data/database.db')
+    base: Connection = sq.connect('data/database.db')
     cur = base.cursor()
     if base:
         print('Data base connected - OK')
 
 
 async def get_all_details(message):
-    for ret in cur.execute('SELECT DISTINCT Деталь FROM details_operation').fetchall():
-        await bot.send_message(message.from_user.id, *ret)
+    keyboard = InlineKeyboardMarkup(row_width=4, resize_keyboard=True)
+    for ret in cur.execute('''SELECT DISTINCT Деталь FROM details_operation''').fetchall():
+        keyboard.insert(InlineKeyboardButton(f'{ret[0]}', callback_data=f'time_op {ret[0]}'))
+    await bot.send_message(message.from_user.id, text='Детали - нажми что бы узнать время', reply_markup=keyboard)
 
 
-async def get_time_operation(message):
-    for ret in cur.execute(
-            'SELECT "номер операции", "время от БТЗ"  FROM details_operation WHERE Деталь = "148.40.301" AND "время от БТЗ" IS NOT NULL').fetchall():
-        await bot.send_message(message.from_user.id, f'Операция {ret[0]} - {ret[1]} минут')
+async def get_time_operation(callback_query, detail):
+    text_massage = ''
+    await bot.send_message(callback_query.from_user.id, detail)
+
+    ret = cur.execute('''SELECT "номер операции", "время от БТЗ" 
+                            FROM details_operation 
+                            WHERE Деталь = ? AND "время от БТЗ" IS NOT NULL''', (detail,)).fetchall()
+    for i in ret:
+        text_massage += f'Оп {i[0]} - {i[1]} мин. \n'
+    await bot.send_message(callback_query.from_user.id, text=text_massage)
--- handlers/client.py
+++ handlers/client.py
@@ -3,20 +3,22 @@ from loader import bot
 from keyboards import kb_client
 from data_base import sql_db
 
+
 async def command_start(message: types.Message):
     await bot.send_message(message.from_user.id, 'get start!!!', reply_markup=kb_client)
 
-async  def get_details(message: types.Message):
+
+async def get_details(message: types.Message):
     await sql_db.get_all_details(message)
 
 
-async  def get_time_detail(message: types.Message):
-    await sql_db.get_time_operation(message)
+async def get_time_detail(callback_query: types.CallbackQuery):
+    detail = callback_query.data.replace('time_op ', '')
+    await sql_db.get_time_operation(callback_query, detail)
 
 
 def register_handlers_client(dp: Dispatcher):
     dp.register_message_handler(command_start, commands=['start'])
     dp.register_message_handler(get_details, commands=['Детали'])
-    dp.register_message_handler(get_time_detail, commands=['40.301'])
-    # dp.register_message_handler(echo_send)
-    # dp.register_message_handler(get_machine_time, commands=['start', 'help'])
+    dp.register_callback_query_handler(get_time_detail, lambda x: x.data and x.data.startswith('time_op '))
+
