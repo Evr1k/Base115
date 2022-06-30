31458e0d57e270d67064b23dcada46d5232d1954
Viktor Plekhanov <evr1k@mail.ru>
2022-06-30 16:38:03 +0500

    test

--- data_base/sql_db.py
+++ data_base/sql_db.py
@@ -1,6 +1,7 @@
 import sqlite3 as sq
 from loader import bot
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from data.config import admin_id
 
 
 def sql_start():
@@ -18,6 +19,8 @@ async def get_all_details(message):
     for ret in cur.execute('''SELECT DISTINCT Деталь FROM details_operation''').fetchall():
         keyboard.insert(InlineKeyboardButton(f'{ret[0]}', callback_data=f'time_op {ret[0]}'))
     await bot.send_message(message.from_user.id, text='Детали - нажми что бы узнать время', reply_markup=keyboard)
+    await bot.send_message(admin_id, f'Create qwerry from \
+                            {message.from_user.first_name} {message.from_user.last_name}!!!')
 
 
 async def get_time_operation(callback_query, detail):
@@ -25,9 +28,9 @@ async def get_time_operation(callback_query, detail):
     text_massage = ''
     await bot.send_message(callback_query.from_user.id, detail)
 
-    ret = cur.execute('''SELECT "номер операции", "время от БТЗ" 
+    ret = cur.execute('''SELECT "номер операции", "название операции", "время от БТЗ" 
                             FROM details_operation 
                             WHERE Деталь = ? AND "время от БТЗ" IS NOT NULL''', (detail,)).fetchall()
     for i in ret:
-        text_massage += f'Оп {i[0]} - {i[1]} мин. \n'
+        text_massage += f'Оп {i[0]} - {i[1]} - {i[2]} мин. \n'
     await bot.send_message(callback_query.from_user.id, text=text_massage)
--- main.py
+++ main.py
@@ -13,4 +13,4 @@ client.register_handlers_client(dp)
 admin.register_handlers_admin(dp)
 
 if __name__ == '__main__':
-    executor.start_polling(dp, on_startup=on_startup())
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup())
