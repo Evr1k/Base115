bb470564bf87d494a52c82b48d57bd3060660637
Viktor Plekhanov <evr1k@mail.ru>
2023-01-29 11:03:10 +0500

    исправление ошибок

--- data/config.py
+++ data/config.py
@@ -1,5 +1,4 @@
-TOKEN = '5542990219:AAGIID8Ajz96C3-4lvT0QmN5RJL1TFoueYk' # test bot
-# TOKEN = '5128369612:AAG5xYo87X0N7sO35k0NrJwB5FX17TDWpCo'  # master bot
-admin_id = '1889647423'
+import os
 
-# https://github.com/Yarolf/async-telegram-bot-with-webhooks-on-heroku/tree/master/heroku-test
+
+token = os.getenv("TOKEN")
--- loader.py
+++ loader.py
@@ -1,6 +1,6 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-import sqlite3 as sq
+
 
 from data.config import TOKEN
 bot = Bot(TOKEN)
